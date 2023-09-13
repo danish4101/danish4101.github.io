28efb0f8117cc9b24029d46ec44ea85e71090ddc
danish <danishwho_@MacBook-Air.local>
2023-09-13 04:21:38 -0400

    added links to navbar and updated about me

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Web Developer",
+    "Graphic Designer",
+    "Web Developer",
+    "Video Editor",
+    "Web Developer",
+  ],
+  typeSpeed: 60,
+});
